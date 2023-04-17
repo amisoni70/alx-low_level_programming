e5013fec40cb84af47a6b6aa1ca95fa0cde955da
amisoni70 <amisoni7014@gmail.com>
2023-04-17 05:29:15 -0700

    structure that defines different types of information for a dog

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+This is my practice for structs and typedef in c
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog- structure that contains the type of details of the dog
+ * @name: first member
+ * @age: second member
+ * @owner: third member
+ *
+ * Description: Contains the dog's name, age and name of the owner
+**/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
