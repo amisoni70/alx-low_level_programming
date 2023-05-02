c7588c360633ae5610d909eb621cdf1722bfb4c4
amisoni70 <amisoni7014@gmail.com>
2023-05-02 06:55:22 -0700

    function that frees memory of the nodes

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_listint2 - function that frees a list
+ * @head: pointer to the listint_t that needs to be freed
+**/
+
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	if (head == NULL)
+		return;
+
+	while (*head)
+	{
+		temp = (*head)->next;
+		free(*head);
+		*head = temp;
+	}
+	*head = NULL;
+}
