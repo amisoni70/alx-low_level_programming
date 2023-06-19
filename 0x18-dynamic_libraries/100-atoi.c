51daa9053a50fe83ffbcdb55f4bd6ca0a6600b12
amisoni70 <amisoni7014@gmail.com>
2023-06-19 06:54:59 -0700

    Task 0

---
+++ 0x18-dynamic_libraries/100-atoi.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _atoi - function that converts a digit into a character
+ * @s: character to be returned
+ * Return: the character s
+**/
+int _atoi(char *s)
+{
+	return (*s);
+}
